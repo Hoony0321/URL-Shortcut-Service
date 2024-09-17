fba620acf9c2338e8038de39bc8b0f8b0b02248a
Hoony0321 <eogns0321@gmail.com>
2024-09-16 17:26:49 -0700

    init project

---
+++ src/test/java/com/hoony0321/URL_Shortcut/UrlShortcutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoony0321.URL_Shortcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortcutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
